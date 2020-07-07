2a2769e5d108d6e17b8b745e7302689b691f3d9d
Aneesh1124 <64183898+Aneesh1124@users.noreply.github.com>
2020-07-07 19:02:17 +0530

    Add files via upload

---
+++ synchronousBallMovement-master/game.js
@@ -0,0 +1,50 @@
+class Game{
+    constructor(){
+
+}
+    getState(){
+    var gameStateRef = database.ref('gameState')
+    gameStateRef.on("value",function(data){
+    gameState = data.val();
+}
+)
+}
+    update(state){
+    database.ref('/').update({
+    gameState:state
+}
+)
+}
+    async start(){
+    if(gameState == 0){
+        player = new Player();
+        player.getCount();
+        form = new Form();
+        form.display();
+    }
+   }
+  play(){
+  form.hide;
+  textSize(30)
+  text("gameStart",120 ,100)
+  Player.getplayerinfo();
+  if(allplayers == defined){
+      var displayPosition = 130
+      for(var plr in allplayers){
+      if(plr == "player"+player.index){
+          fill("red")
+      }
+      else {
+        fill("black")
+      }
+      displayPosition = displayPosition+20
+      textSize(15)
+      text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+  }
+ }
+if(keyDown(UP_ARROW)&&(player.index == null)){
+    player.distance = player.distance + 50
+    player.update();
+}
+}
+}
